9f55a6e816113481383e9205dd068153a78cd08c
jpfreeley <jpf321@gmail.com>
2018-02-27 22:01:31 -0500

    created twitter stream capture

---
+++ config_template.py
@@ -0,0 +1,16 @@
+
+# COPY THIS FILE AS "config.py" and edit the values as needed
+
+# Login to twitter
+# http://apps.twitter.com
+# Create new app 
+# -- URL can be placeholder
+# -- Callback should be left blacnk
+# Click link to "manage keys and access tokens"
+# Click button to "Generate Tokens"
+# DOCS: https://developer.twitter.com/en/docs/basics/authentication/guides/access-tokens
+
+CONSUMER_KEY = 'Your Consumer Key'
+CONSUMER_SECRET = 'Your Consumer Secret'
+ACCESS_TOKEN = 'Your Access Token'
+ACCESS_SECRET = 'Your Access Secret'
